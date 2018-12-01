cba0c8a6dbf757a87c7e300436c2763ac1746180
Наталия Алексеева <nn2005n@rambler.ru>
2018-12-01 23:08:07 +0300

    Окно настройки

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,8 @@
 'use strict';
+
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var firstNames = [
   'Иван',
   'Хуан Себастьян',
@@ -78,3 +82,44 @@ for (i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// Открытие/закрытие окна настройки персонажа
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
