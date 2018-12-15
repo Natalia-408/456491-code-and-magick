c8c78ed36333364c842d04b3bfd9e5320a1dbc4e
Наталия Алексеева <nn2005n@rambler.ru>
2018-12-15 21:48:04 +0300

    Сервер. Начало

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-
+/*
   var firstNames = [
     'Иван',
     'Хуан Себастьян',
@@ -33,6 +33,7 @@
   var colorEyes = ['black', 'red', 'blue', 'yellow', 'green'];
 
   var colorfireballs = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
 
   var userDialog = document.querySelector('.setup');
   userDialog.classList.remove('hidden');
@@ -119,4 +120,47 @@
   };
 
   wizardFireball.addEventListener('click', fireballClickHandler);
+*/
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+  
+  return wizardElement;
+};
+
+var successHandler = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  
+  for (var i = 0; i < 4; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+};
+
+var errorHandler = function (errorMessage) {
+  var node = document.createElement('div');
+  node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+  node.style.position = 'absolute';
+  node.style.left = 0;
+  node.style.right = 0;
+  node.style.fontSize = '30px';
+  
+  node.textContent = errorMessage; 
+  document.body.insertAdjacentElement('afterbegin', node);
+};
+
+window.load(successHandler, errorHandler);
+
 })();
